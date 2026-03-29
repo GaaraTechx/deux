0a891d65e0c943070b4c06a821a980adaf846198
GaaraTechx <edwincheerrisaint@gmail.com>
2026-03-29 12:18:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10803292
+    "zoneId": 10803372
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw
